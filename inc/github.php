b0ebfea67627263b0831ff07b0b1ab1e8bb88fd7
Alan Smith <alan@cloakedcode.com>
2011-06-28 11:58:25 -0400

    GitHub repos are now sorted by last pushed date.

--- inc/github.php
+++ inc/github.php
@@ -2,14 +2,15 @@
 preg_match('|github.com/([^/]+)|i', $service['url'], $matches);
 $github_account = $matches[1];
 ?>
+<script type='text/javascript' src='jquery.sort-1.1.js'></script>
 <script type='text/javascript'>
 $(function() {
     $.getJSON('https://api.github.com/users/<?php echo $github_account ?>/repos?callback=?', function(resp) {
         if (resp.data.length > 0) {
             $('#github-repos').append('<ul></ul>');
 
-            $.each(resp.data.reverse(), function(i, val) {
-                $('#github-repos > ul').prepend(
+            $.each($(resp.data).sort('pushed_at', 'desc'), function(i, val) {
+                $('#github-repos > ul').append(
                     '<li><a href="'+val['html_url']+'">'+val['name']+'</a><p>'+((val['description'].length > 0) ? val['description'] : '(No description.)')+'</p></li>'
                 );
             });
