345321e23eb41152e696a8a0675b319c28e12aa2
Alan Smith <alan@cloakedcode.com>
2011-06-28 12:08:51 -0400

    Decodes unicode (utf8) and converts it to human-readable characters.

--- index.php
+++ index.php
@@ -19,7 +19,8 @@ else
 {
     $user = $user->resp;
 
-    $user->bio = (preg_match('/^\s+$/i', $user->bio) ? "(No bio.)" : htmlspecialchars_decode(str_ireplace('/people/', '/forrst/', $user->bio)));
+    $user->name = charset_decode_utf_8($user->name);
+    $user->bio = (preg_match('/^\s+$/i', $user->bio) ? "(No bio.)" : charset_decode_utf_8(str_ireplace('/people/', '/forrst/', $user->bio)));
     $user->is_a = ucwords($user->is_a);
 
     $matches = array();
@@ -56,3 +57,26 @@ else
 }
 
 include('layout.php');
+
+/**
+ * Grabbed this from a comment: http://us2.php.net/manual/en/function.utf8-decode.php#85034
+ * Comes from Squirrelmail.
+ */
+function charset_decode_utf_8 ($string) {
+    /* Only do the slow convert if there are 8-bit characters */
+    /* avoid using 0xA0 (\240) in ereg ranges. RH73 does not like that */
+    if (! ereg("[\200-\237]", $string) and ! ereg("[\241-\377]", $string))
+        return $string;
+
+    // decode three byte unicode characters
+    $string = preg_replace("/([\340-\357])([\200-\277])([\200-\277])/e",       
+            "'&#'.((ord('\\1')-224)*4096 + (ord('\\2')-128)*64 + (ord('\\3')-128)).';'",   
+            $string);
+
+    // decode two byte unicode characters
+    $string = preg_replace("/([\300-\337])([\200-\277])/e",
+            "'&#'.((ord('\\1')-192)*64+(ord('\\2')-128)).';'",
+            $string);
+
+    return $string;
+} 
